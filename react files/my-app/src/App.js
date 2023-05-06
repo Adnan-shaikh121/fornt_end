c3b69a4b680516da86f803c80852bd9490854e74
Adnan_shaikh <adnan.sits.mech@gmail.com>
2023-05-06 22:43:02 +0530

    practice file

--- react files/my-app/src/App.js
+++ react files/my-app/src/App.js
@@ -1,48 +1,13 @@
-
-
+import TextForm from './components/textForm';
 import './App.css';
+import Navbar from './components/navbar';
 function App() {
   return (
     <>
-      <nav className="navbar navbar-expand-lg bg-body-tertiary">
-        <div className="container-fluid">
-          <a className="navbar-brand" href="https://www.example.com">STUDY SONIC</a>
-          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-          </button>
-          <div className="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="https://www.example.com">Home</a>
-              </li>
-              <li className='nav-item'>
-                <a className='nav-link active' aria-current="page" href='https://www.example.com'>Universities</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="https://www.example.com">Year</a>
-              </li>
-              <li className="nav-item">
-                <a className="nav-link" href="https://www.example.com">About</a>
-                <ul className="dropdown-menu">
-                  <li><a className="dropdown-item" href="https://www.example.com">Action</a></li>
-                  <li><a className="dropdown-item" href="https://www.example.com">Another action</a></li>
-                  <li><hr className="dropdown-divider"/></li>
-                  <li><a className="dropdown-item" href="https://www.example.com">Something else here</a></li>
-                </ul>
-              </li>
-              <li className="nav-item">
-                <a href="https://www.example.com" className="nav-link disabled">Disabled</a>
-              </li>
-            </ul>
-            <form className="d-flex" role="search">
-              <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-                <button className="btn btn-outline-success" type="submit">Search</button>
-            </form>
-          </div>
-        </div>
-      </nav>
+    {/* <Navbar title="STUDY SONIC" aboutText="About" thisIsCheck="just Cheking" /> */}
+    <Navbar/>
+    <TextForm heading="text from props"/>
     </>
   );
 }
-
 export default App;
