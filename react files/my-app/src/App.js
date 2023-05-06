7745abf94b0a000bc442e75228d7bffb9b0bbc92
Adnan_shaikh <adnan.sits.mech@gmail.com>
2023-05-06 08:40:01 +0530

    practice file

--- react files/my-app/src/App.js
+++ react files/my-app/src/App.js
@@ -6,7 +6,7 @@ function App() {
     <>
       <nav className="navbar navbar-expand-lg bg-body-tertiary">
         <div className="container-fluid">
-          <a className="navbar-brand" href="https://www.example.com">Navbar</a>
+          <a className="navbar-brand" href="https://www.example.com">STUDY SONIC</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
@@ -14,6 +14,12 @@ function App() {
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 <a className="nav-link active" aria-current="page" href="https://www.example.com">Home</a>
+              </li>
+              <li className='nav-item'>
+                <a className='nav-link active' aria-current="page" href='https://www.example.com'>Universities</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="https://www.example.com">Year</a>
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="https://www.example.com">About</a>
