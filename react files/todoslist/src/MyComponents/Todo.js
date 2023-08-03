6447c7cc0750df129b8a44ae34f9b92169975a36
Adnan_shaikh <107258786+Adnan-shaikh121@users.noreply.github.com>
2023-08-03 10:49:12 +0530

    second commit

---
+++ react files/todoslist/src/MyComponents/Todo.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Todo=(Todos)=>{
+    return(
+        <div>
+            <h4>
+                this is h4 from TODO
+            </h4>
+            <div>
+                {/* {Todos} */}
+            </div>
+        </div>
+        
+    )
+}
---
+++ react files/todoslist/src/MyComponents/Todos.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Todo } from "./Todo";
+
+export function Todos(props){
+    return (
+        <div>
+            <Todo Todo={Todos}/>
+        </div>
+    )
+}
