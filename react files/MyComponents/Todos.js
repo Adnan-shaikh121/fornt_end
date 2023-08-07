df7a80c1f8129dcf73086d4277ba9ee2a021c9a5
Adnan_shaikh <107258786+Adnan-shaikh121@users.noreply.github.com>
2023-08-07 19:37:23 +0530

    odified

--- react files/MyComponents/Todos.js
+++ react files/MyComponents/Todos.js
@@ -4,7 +4,8 @@ import { Todo } from "./Todo";
 export function Todos(props){
     return (
         <div className="conatiner text-center">
-            <h1 className="my-4">Todo List</h1>
+            <h1 className="text-center my-4">Todo List</h1>
+            <h1>this is H1</h1>
             <Todo/>
 
         </div>
