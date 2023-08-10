c2fc855a11e2336e29c58c052ed4f7db29721e61
Adnan_shaikh <107258786+Adnan-shaikh121@users.noreply.github.com>
2023-08-10 18:19:17 +0530

    third commit

---
+++ react files/todolist/src/MyComponents/Todos.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Todo } from "./Todo";
+
+export function Todos(props){
+    return (
+        <div className="conatiner text-center">
+            <h1 className="text-center my-4">Todo List</h1>
+            <h1>this is H1</h1>
+            <Todo/>
+
+        </div>
+    )
+}
